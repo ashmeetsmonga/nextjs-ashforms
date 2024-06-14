502dc288233a2f2457b158f387cb6d6f7a004765
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-06-14 12:15:02 +0530

    Added editable form with dynamic fields

---
+++ app/forms/page.tsx
@@ -0,0 +1,12 @@
+import { Input } from "@/components/ui/input";
+import React from "react";
+
+const FormsPage = () => {
+  return (
+    <div>
+      <Input placeholder="Add Name" />
+    </div>
+  );
+};
+
+export default FormsPage;
