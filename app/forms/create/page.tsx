281e4bcbef6bf718da57b57055bfefb050cb3c5a
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-06-18 00:29:36 +0530

    Editable form styling done

--- app/forms/create/page.tsx
+++ app/forms/create/page.tsx
@@ -4,6 +4,7 @@ import EditableForm from "@/components/EditableForm/EditableForm";
 import ViewForm from "@/components/ViewForm";
 import { Button } from "@/components/ui/button";
 import { FormDetails } from "@/lib/types";
+import { Eye } from "lucide-react";
 import React, { useState } from "react";
 
 const page = () => {
@@ -14,9 +15,14 @@ const page = () => {
   const [previewForm, setPreviewForm] = useState(false);
 
   return (
-    <div>
-      <Button onClick={() => setPreviewForm((prev) => !prev)}>Preview</Button>
-      {!previewForm ? <EditableForm formDetails={formDetails} setFormDetails={setFormDetails} mode="create" /> : <ViewForm formDetails={formDetails} mode="preview" />}
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col w-full max-w-[1000px]">
+        <Button className="self-end" variant="ghost" onClick={() => setPreviewForm((prev) => !prev)}>
+          <Eye className="mr-2" />
+          Preview
+        </Button>
+        {!previewForm ? <EditableForm formDetails={formDetails} setFormDetails={setFormDetails} mode="create" /> : <ViewForm formDetails={formDetails} mode="preview" />}
+      </div>
     </div>
   );
 };
--- components/EditableForm/EditableForm.tsx
+++ components/EditableForm/EditableForm.tsx
@@ -111,7 +111,7 @@ const EditableForm: FC<EditableFormProps> = ({ formDetails, setFormDetails, mode
       <div className="flex flex-col gap-6">
         <Input
           placeholder="Enter form title"
-          className="text-3xl font-bold p-8 pl-0 border-0 focus-visible:ring-0"
+          className="text-5xl font-semibold tracking-tight p-8 pl-0 border-0 focus-visible:ring-0"
           value={formDetails.title}
           type="text"
           onChange={(e) => setFormDetails((prev) => ({ ...prev, title: e.target.value }))}
@@ -125,40 +125,49 @@ const EditableForm: FC<EditableFormProps> = ({ formDetails, setFormDetails, mode
               <Trash2 className="mt-2 text-muted-foreground hover:text-black cursor-pointer transition-colors" onClick={() => handleDelete(idx)} />
               {ques.type === "text" && (
                 <div className="flex flex-col gap-2 flex-1">
-                  <Input placeholder="Enter the question" value={ques.title} onChange={(e) => handleInputChange(e.target.value, idx, "title")} />
-                  <Input placeholder="Enter the placeholder" value={ques.placeholder} className="text-muted-foreground" onChange={(e) => handleInputChange(e.target.value, idx, "placeholder")} />
+                  <Input placeholder="Enter the question" value={ques.title} onChange={(e) => handleInputChange(e.target.value, idx, "title")} className="border-none font-semibold text-xl" />
+                  <Input
+                    placeholder="Enter a placeholder for the response"
+                    value={ques.placeholder}
+                    className="text-muted-foreground ml-2 w-1/2"
+                    onChange={(e) => handleInputChange(e.target.value, idx, "placeholder")}
+                  />
                 </div>
               )}
               {ques.type === "radio" && (
                 <div className="flex flex-col gap-2 flex-1">
-                  <Input placeholder="Enter the question" value={ques.title} onChange={(e) => handleInputChange(e.target.value, idx, "title")} />
-                  {ques.options?.map((option, optionIdx) => (
-                    <div key={optionIdx} className="flex gap-2 items-center">
-                      {optionIdx === 0 ? (
-                        <Plus size={28} className="text-muted-foreground hover:text-black cursor-pointer transition-colors" onClick={() => addNewRadioOption(idx)} />
-                      ) : (
-                        <Trash2 className="ml-1 text-muted-foreground hover:text-black cursor-pointer transition-colors" onClick={() => deleteRadioOption(optionIdx, idx)} />
-                      )}
-                      <Circle />
-                      <Input placeholder="Enter Option" value={option} onChange={(e) => handleRadioOptionChange(e.target.value, optionIdx, idx)} />
-                    </div>
-                  ))}
+                  <Input placeholder="Enter the question" value={ques.title} onChange={(e) => handleInputChange(e.target.value, idx, "title")} className="border-none font-semibold text-xl" />
+                  <div>
+                    {ques.options?.map((option, optionIdx) => (
+                      <div key={optionIdx} className="flex gap-2 items-center">
+                        {optionIdx === 0 ? (
+                          <Plus size={28} className="text-muted-foreground hover:text-black cursor-pointer transition-colors" onClick={() => addNewRadioOption(idx)} />
+                        ) : (
+                          <Trash2 className="ml-1 text-muted-foreground hover:text-black cursor-pointer transition-colors" onClick={() => deleteRadioOption(optionIdx, idx)} />
+                        )}
+                        <Circle />
+                        <Input placeholder="Enter Option" value={option} onChange={(e) => handleRadioOptionChange(e.target.value, optionIdx, idx)} className="border-none" />
+                      </div>
+                    ))}
+                  </div>
                 </div>
               )}
               {ques.type === "checkbox" && (
                 <div className="flex flex-col gap-2 flex-1">
-                  <Input placeholder="Enter the question" value={ques.title} onChange={(e) => handleInputChange(e.target.value, idx, "title")} />
-                  {ques.options?.map((option, optionIdx) => (
-                    <div key={optionIdx} className="flex gap-2 items-center">
-                      {optionIdx === 0 ? (
-                        <Plus size={28} className="text-muted-foreground hover:text-black cursor-pointer transition-colors" onClick={() => addNewCheckboxOption(idx)} />
-                      ) : (
-                        <Trash2 className="ml-1 text-muted-foreground hover:text-black cursor-pointer transition-colors" onClick={() => deleteCheckboxOption(optionIdx, idx)} />
-                      )}
-                      <Square />
-                      <Input placeholder="Enter Option" value={option} onChange={(e) => handleCheckboxOptionChange(e.target.value, optionIdx, idx)} />
-                    </div>
-                  ))}
+                  <Input placeholder="Enter the question" value={ques.title} onChange={(e) => handleInputChange(e.target.value, idx, "title")} className="border-none font-semibold text-xl" />
+                  <div>
+                    {ques.options?.map((option, optionIdx) => (
+                      <div key={optionIdx} className="flex gap-2 items-center">
+                        {optionIdx === 0 ? (
+                          <Plus size={28} className="text-muted-foreground hover:text-black cursor-pointer transition-colors" onClick={() => addNewCheckboxOption(idx)} />
+                        ) : (
+                          <Trash2 className="ml-1 text-muted-foreground hover:text-black cursor-pointer transition-colors" onClick={() => deleteCheckboxOption(optionIdx, idx)} />
+                        )}
+                        <Square />
+                        <Input placeholder="Enter Option" value={option} onChange={(e) => handleCheckboxOptionChange(e.target.value, optionIdx, idx)} className="border-none" />
+                      </div>
+                    ))}
+                  </div>
                 </div>
               )}
             </div>
