2bf3bca8739844e1bd4d510e66339eea5b5cb862
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-06-19 22:06:48 +0530

    Login Register form ui changed

--- components/LoginDialog.tsx
+++ components/LoginDialog.tsx
@@ -12,13 +12,16 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { LoginPayload } from "@/lib/types";
-import { loginUser } from "@/app/actions/userActions";
+import { LoginPayload, RegisterPayload } from "@/lib/types";
+import { loginUser, registerUser } from "@/app/actions/userActions";
 import { useRouter } from "next/navigation";
 import { useRecoilState } from "recoil";
 import { userAtom } from "@/app/recoil/atom/userAtom";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 
 const LoginDialog = () => {
   const router = useRouter();
@@ -30,7 +33,7 @@ const LoginDialog = () => {
     setIsOpen(true);
   }, []);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleLoginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const payload: LoginPayload = {
@@ -47,19 +50,75 @@ const LoginDialog = () => {
       .catch((e) => console.log(e));
   };
 
+  const handleRegisterSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const payload: RegisterPayload = {
+      name: "",
+      email: "",
+      password: "",
+    };
+    for (let [key, value] of formData.entries()) payload[key as keyof RegisterPayload] = value as string;
+    registerUser(payload)
+      .then((data) => {
+        console.log(data);
+        router.push("/forms");
+      })
+      .catch((e) => console.log(e));
+  };
+
   return (
     <AlertDialog open={isOpen}>
       <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Login</AlertDialogTitle>
-          <AlertDialogDescription asChild>
-            <form onSubmit={handleSubmit} className="flex gap-4 flex-col">
-              <Input name="email" placeholder="Email" />
-              <Input name="password" placeholder="Password" />
-              <Button>Login</Button>
-            </form>
-          </AlertDialogDescription>
-        </AlertDialogHeader>
+        <Tabs defaultValue="login">
+          <TabsList className="w-full">
+            <TabsTrigger className="w-full" value="login">
+              Login
+            </TabsTrigger>
+            <TabsTrigger className="w-full" value="register">
+              Register
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <AlertDialogDescription asChild>
+              <Card>
+                <CardHeader>
+                  <CardTitle className="text-black">Login</CardTitle>
+                  <CardDescription className="pt-1">Login into your account. Create a new account if you do not have one.</CardDescription>
+                </CardHeader>
+                <CardContent className="h-[250px]">
+                  <form onSubmit={handleLoginSubmit} className="flex gap-4 flex-col justify-between items-end h-full">
+                    <div className="space-y-4 w-full text-black">
+                      <Input name="email" placeholder="Email" />
+                      <Input name="password" type="password" placeholder="Password" />
+                    </div>
+                    <Button className="min-w-fit px-8">Login</Button>
+                  </form>
+                </CardContent>
+              </Card>
+            </AlertDialogDescription>
+          </TabsContent>
+          <TabsContent value="register">
+            <AlertDialogDescription asChild>
+              <Card>
+                <CardHeader>
+                  <CardTitle className="text-black">Register</CardTitle>
+                  <CardDescription className="pt-1">Create a new account. Login into your account if you already have one</CardDescription>
+                </CardHeader>
+                <CardContent className="h-[250px]">
+                  <form onSubmit={handleRegisterSubmit} className="flex gap-4 flex-col items-end justify-between h-full">
+                    <div className="space-y-4 w-full text-black">
+                      <Input name="name" placeholder="Name" />
+                      <Input name="email" placeholder="Email" />
+                      <Input name="password" placeholder="Password" />
+                    </div>
+                    <Button className="min-w-fit px-8">Register</Button>
+                  </form>
+                </CardContent>
+              </Card>
+            </AlertDialogDescription>
+          </TabsContent>
+        </Tabs>
       </AlertDialogContent>
     </AlertDialog>
   );
