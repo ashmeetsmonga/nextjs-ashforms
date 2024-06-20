5de50767e44e0a530c3079c5781908cc2b75047f
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-06-21 00:22:51 +0530

    Added api in edit form page

--- app/forms/[slug]/page.tsx
+++ app/forms/[slug]/page.tsx
@@ -1,18 +1,26 @@
 "use client";
 
+import { getFormByID } from "@/app/actions/formActions";
 import { formAtom } from "@/app/recoil/atom/formAtom";
 import EditableForm from "@/components/EditableForm/EditableForm";
 import { FormDetails } from "@/lib/types";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 
 const page = ({ params }: { params: { slug: string } }) => {
-  const formRecoilState = useRecoilValue(formAtom);
-  const selectedForm = formRecoilState.forms.filter((form) => form.id === params.slug)[0];
   const [formDetails, setFormDetails] = useState<FormDetails>({
-    title: selectedForm.title,
-    questions: JSON.parse(selectedForm.questions),
+    title: "",
+    questions: [],
   });
+
+  useEffect(() => {
+    getFormByID(params.slug).then((data: any) => {
+      setFormDetails({
+        title: data.data.form.title,
+        questions: data.data.questions,
+      });
+    });
+  }, []);
 
   return <EditableForm formDetails={formDetails} setFormDetails={setFormDetails} mode="edit" id={params.slug} />;
 };
