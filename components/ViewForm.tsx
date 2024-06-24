20ad6e2d2320b9d34ae0b8c723101f74059f29ba
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-06-25 00:14:49 +0530

    added alert in successful form submission

--- components/ViewForm.tsx
+++ components/ViewForm.tsx
@@ -1,5 +1,5 @@
 import { FormDetails, ResponsePayload } from "@/lib/types";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Input } from "./ui/input";
 import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
 import { Checkbox } from "./ui/checkbox";
@@ -7,6 +7,8 @@ import { Button } from "./ui/button";
 import { addResponse } from "@/app/actions/responseActions";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Rocket } from "lucide-react";
 
 interface ViewFormProps {
   formDetails: FormDetails;
@@ -14,6 +16,7 @@ interface ViewFormProps {
   mode: "preview" | "view";
 }
 const ViewForm: FC<ViewFormProps> = ({ formDetails, formId, mode }) => {
+  const [isFormSubmittedSuccessfully, setIsFormSubmittedSuccessfully] = useState(false);
   const router = useRouter();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -34,8 +37,8 @@ const ViewForm: FC<ViewFormProps> = ({ formDetails, formId, mode }) => {
     };
     addResponse(payload)
       .then((data) => {
-        toast.success("Response Submitted Successfully", { id: toastID });
-        router.push("/forms");
+        toast.dismiss(toastID);
+        setIsFormSubmittedSuccessfully(true);
       })
       .catch((e) => {
         console.log(e);
@@ -46,44 +49,53 @@ const ViewForm: FC<ViewFormProps> = ({ formDetails, formId, mode }) => {
   return (
     <div className="flex flex-col gap-6">
       <h1 className="text-5xl font-bold mb-10">{formDetails.title}</h1>
-      <form className="space-y-6" onSubmit={handleSubmit}>
-        {formDetails.questions.map((ques, idx) => (
-          <>
-            {ques.type === "text" && (
-              <div className="space-y-2">
-                <p className="text-xl font-semibold">{ques.title}</p>
-                <Input name={idx.toString()} placeholder={ques.placeholder} className="border-none text bg-gray-100" />
-              </div>
-            )}
-            {ques.type === "radio" && (
-              <div className="space-y-2">
-                <p className="text-xl font-semibold">{ques.title}</p>
-                <RadioGroup name={idx.toString()}>
-                  {ques.options?.map((option, idx) => (
+      {isFormSubmittedSuccessfully && (
+        <Alert>
+          <Rocket className="h-4 w-4" />
+          <AlertTitle>Submission Successful! </AlertTitle>
+          <AlertDescription>Your form has been submitted successfully. We appreciate your input & will process it shortly. Thank you for your time</AlertDescription>
+        </Alert>
+      )}
+      {!isFormSubmittedSuccessfully && (
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          {formDetails.questions.map((ques, idx) => (
+            <>
+              {ques.type === "text" && (
+                <div className="space-y-2">
+                  <p className="text-xl font-semibold">{ques.title}</p>
+                  <Input name={idx.toString()} placeholder={ques.placeholder} className="border-none text bg-gray-100" />
+                </div>
+              )}
+              {ques.type === "radio" && (
+                <div className="space-y-2">
+                  <p className="text-xl font-semibold">{ques.title}</p>
+                  <RadioGroup name={idx.toString()}>
+                    {ques.options?.map((option, idx) => (
+                      <div key={option} className="flex gap-2 items-center">
+                        <RadioGroupItem value={option} />
+                        <p>{option}</p>
+                      </div>
+                    ))}
+                  </RadioGroup>
+                </div>
+              )}
+              {ques.type === "checkbox" && (
+                <div className="space-y-2">
+                  <p className="text-xl font-semibold">{ques.title}</p>
+                  {ques.options?.map((option, checkboxIdx) => (
                     <div key={option} className="flex gap-2 items-center">
-                      <RadioGroupItem value={option} />
-                      <p>{option}</p>
+                      <Checkbox name={idx.toString()} id={option} value={option} />
+                      <label htmlFor={option}>{option}</label>
                     </div>
                   ))}
-                </RadioGroup>
-              </div>
-            )}
-            {ques.type === "checkbox" && (
-              <div className="space-y-2">
-                <p className="text-xl font-semibold">{ques.title}</p>
-                {ques.options?.map((option, checkboxIdx) => (
-                  <div key={option} className="flex gap-2 items-center">
-                    <Checkbox name={idx.toString()} id={option} value={option} />
-                    <label htmlFor={option}>{option}</label>
-                  </div>
-                ))}
-              </div>
-            )}
-          </>
-        ))}
+                </div>
+              )}
+            </>
+          ))}
 
-        {mode === "view" && <Button type="submit">Submit</Button>}
-      </form>
+          {mode === "view" && <Button type="submit">Submit</Button>}
+        </form>
+      )}
     </div>
   );
 };
